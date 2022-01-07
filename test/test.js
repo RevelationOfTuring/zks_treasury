e2a16793200c49e922a4278ca0dd3f04f1d718a4
MichaelWang <wangguanyi@web3.com>
2022-01-07 14:49:24 +0800

    erc20 init

--- test/test.js
+++ test/test.js
@@ -3,13 +3,16 @@ const {ethers} = require("hardhat");
 
 describe("ZKSTreasury", function () {
     let mockZkCore, erc20Token, zksTreasury
-    let deployer, receiverL2
+    let deployer, receiverL2, user
     let maxEthDeposit = "100"
+    let nativeETHAddress = '0x0000000000000000000000000000000000000000'
 
     before(async () => {
-        [deployer, receiverL2] = await ethers.getSigners()
-        console.log('deployer address: ' + deployer.address)
-        console.log('receiver on layer 2 address: ' + receiverL2.address)
+        [deployer, receiverL2, user] = await ethers.getSigners()
+        console.log(`deployer address: ${deployer.address}`)
+        console.log(`receiver on layer 2 address: ${receiverL2.address}`)
+        console.log(`user address: ${user.address}`)
+        console.log('deployer eth balance: ' + await ethers.provider.getBalance(deployer.address))
         const MockZkCore = await ethers.getContractFactory("MockZKCore")
         const Erc20Token = await ethers.getContractFactory("MockErc20Token")
         const ZKSTreasury = (await ethers.getContractFactory("ZKSTreasury")).connect(deployer)
@@ -18,20 +21,26 @@ describe("ZKSTreasury", function () {
         erc20Token = await Erc20Token.deploy()
         await mockZkCore.deployed()
         await erc20Token.deployed()
-        console.log('mock ZkCore contract: ' + mockZkCore.address)
-        console.log('mock Erc20 contract: ' + erc20Token.address)
+        console.log(`mock ZkCore contract: ${mockZkCore.address}`)
+        console.log(`mock Erc20 contract: ${erc20Token.address}`)
 
         zksTreasury = await ZKSTreasury.deploy(maxEthDeposit, receiverL2.address, mockZkCore.address)
         await zksTreasury.deployed()
-        console.log('zks treasury contract: ' + zksTreasury.address)
+        console.log(`zks treasury contract: ${zksTreasury.address}`)
 
         expect(await zksTreasury.owner()).to.equal(deployer.address)
         expect(await zksTreasury.zkCoreAddress()).to.equal(mockZkCore.address)
         expect(await zksTreasury.receiverLayer2()).to.equal(receiverL2.address)
+        expect(await zksTreasury.maxDepositAmount(nativeETHAddress)).to.equal(maxEthDeposit)
+        console.log('deployer eth balance: ' + await ethers.provider.getBalance(deployer.address))
     })
 
-    it("common test", async function () {
-
+    it("erc20 init", async () => {
+        expect(await erc20Token.balanceOf(deployer.address)).to.equal(0)
+        await erc20Token.mintDirectly(user.address, '1000')
+        await erc20Token.mintDirectly(deployer.address, '1000')
+        expect(await erc20Token.balanceOf(user.address)).to.equal(1000)
+        expect(await erc20Token.balanceOf(deployer.address)).to.equal(1000)
     })
 });
 
