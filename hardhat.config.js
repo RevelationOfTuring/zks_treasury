8794961e82a4a2b22e97cc47a16ad2c823bb81c3
MichaelWang <wangguanyi@web3.com>
2022-01-28 16:01:27 +0800

    multi compilers

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,13 +4,27 @@ const PRIVATE_KEY = '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf
 
 module.exports = {
     solidity: {
-        version: "0.8.7",
-        settings: {
-            optimizer: {
-                enabled: true,
-                runs: 200,
+        compilers: [
+            {
+                version: "0.8.7",
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 200,
+                    }
+                },
+            },
+            {
+                version: "0.4.17",
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 200,
+                    }
+                },
             }
-        },
+        ]
+
     },
 
     networks: {
